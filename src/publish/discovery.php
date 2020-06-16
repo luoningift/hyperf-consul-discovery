74330a99ba0f3fe1b69b0ae3043441bdc91e85f4
罗宁 <nluo@kaikeba.com>
2020-06-16 19:59:06 +0800

    注册发现修改

--- src/publish/discovery.php
+++ src/publish/discovery.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 return [
     'consul' => [
         'url' => env('CONSUL_URL', 'http://127.0.0.1:8500'),
-        'enable' => (int) env('CONSUL_ENABLE', 1),
+        'enable' => (int) env('CONSUL_ENABLE', 0),
         'net_card' => env('CONSUL_NET_CARD', ''),
     ],
 ];
